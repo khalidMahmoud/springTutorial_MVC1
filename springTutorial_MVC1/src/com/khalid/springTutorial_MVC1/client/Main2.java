ef7efdcadfc04d531968de46a866afafc24ec683
khalidMahmoud <khalid.mahmoud.basuiny@gmail.com>
2016-02-17 10:32:23 +0200

    firstCommit

---
+++ springTutorial_MVC1/src/com/khalid/springTutorial_MVC1/client/Main2.java
@@ -0,0 +1,5 @@
+package com.khalid.springTutorial_MVC1.client;
+
+public class Main2 {
+
+}
---
+++ springTutorial_MVC1/src/com/khalid/springTutorial_MVC1/client/main.java
@@ -0,0 +1,10 @@
+package com.khalid.springTutorial_MVC1.client;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
